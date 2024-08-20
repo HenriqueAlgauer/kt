59f07e3996c174ba8fe75cea1ff1b21eb5d80e9b
Pia <algauer.henrique@gmail.com>
2024-08-20 19:18:06 -0300

    lake

---
+++ lake/src/main/kotlin/Pato.kt
@@ -0,0 +1,8 @@
+package org.example
+
+class Pato{
+    fun voar(): String{
+        println("Estou voando")
+        return ""
+    }
+}
---
+++ lake/src/test/kotlin/patoTest.kt
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeEach
+import org.example.Pato
+import kotlin.test.assertEquals
+
+class patoTests{
+
+    @get:BeforeEach
+    var patoJuca = Pato()
+
+    @Test
+    fun testeDeVoo(){
+        var patoJo = Pato()
+
+        assertEquals("estou voando", patoJuca.voar())
+    }
+
+}
